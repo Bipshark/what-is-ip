f2af3ee4950be7b867c5e3b4bee9a3d81dfb02dc
Niklas Tegnander <bipshark@gmail.com>
2013-05-24 14:56:37 -0700

    Added before

--- application.rb
+++ application.rb
@@ -10,20 +10,21 @@ set :mustache, {
     :views      => "./views"
 }
 
+before do
+    @ip = request.env["REMOTE_ADDR"]
+end
+
 # If fetched by cURL
 get '/', :agent => /curl/ do
-    @ip = request.env["REMOTE_ADDR"]
     @ip + "\n"
 end
 
 # If fetched by cURL and /n is passed, return IP without new line character
 get '/n', :agent => /curl/ do
-    @ip = request.env["REMOTE_ADDR"]
     @ip
 end
 
 # Default
 get '/' do
-    @ip = request.env["REMOTE_ADDR"]
     mustache :index
 end
