5b67960c027f3a13550d2988604ee62e5f34bfb0
Niklas Tegnander <bipshark@gmail.com>
2013-05-25 22:55:57 -0700

    The website has a design now, yay\!

--- views/index.rb
+++ views/index.rb
@@ -1,7 +1,13 @@
+require "date"
+
 module Views
     class Index < Layout
         def ip
             @ip
+        end
+        def year
+            d = DateTime.now();
+            d.year
         end
     end
 end
